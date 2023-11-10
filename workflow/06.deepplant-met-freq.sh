04c9677e69c551a69ab86c661a3cf3b27ccdcfd5
LIZW2019 <54759510+LIZW2019@users.noreply.github.com>
2023-11-10 16:28:49 +0800

    Update 06.deepplant-met-freq.sh

--- workflow/06.deepplant-met-freq.sh
+++ workflow/06.deepplant-met-freq.sh
@@ -1,6 +1,6 @@
 #06.deepplant-met-freq.sh
-#environment setting, replace $CondaEnv/deepsignalpenv with your actual path
-export PATH=$CondaEnv/deepsignalpenv/bin:$PATH
+#environment setting, replace $MambaEnv/deepsignalpenv with your actual path
+export PATH=$MambaEnv/deepsignalpenv/bin:$PATH
 #calculate frequency
 deepsignal_plant call_freq \
 --input_path ../cache/fast5s.C.call_mods.tsv \
