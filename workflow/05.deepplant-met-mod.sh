5431fb2569a4c60e31ff93feed7e7cc9367539a9
LIZW2019 <54759510+LIZW2019@users.noreply.github.com>
2023-11-10 16:19:58 +0800

    final_v2.3

--- workflow/05.deepplant-met-mod.sh
+++ workflow/05.deepplant-met-mod.sh
@@ -3,7 +3,7 @@
 export PATH=$CondaEnv/deepsignalpenv/bin:$PATH
 #call 5mC
 CUDA_VISIBLE_DEVICES=0,1 deepsignal_plant call_mods \
---input_path ../cache \
+--input_path ../cache/SINGLE_sample_data \
 --model_path ../input/model/model.dp2.CNN.arabnrice2-1_120m_R9.4plus_tem.bn13_sn16.both_bilstm.epoch6.ckpt \
 --result_file ../cache/fast5s.C.call_mods.tsv \
 --corrected_group RawGenomeCorrected_000 \
