9dab0e923816a317d9f1d222c011b9acdab0d1c8
LIZW2019 <54759510+LIZW2019@users.noreply.github.com>
2023-11-10 16:18:22 +0800

    final_v2.3

--- workflow/03.tombo_preprocess.sh
+++ workflow/03.tombo_preprocess.sh
@@ -4,7 +4,7 @@ export PATH=$CondaEnv/deepsignalpenv/bin:$PATH
 # Tombo preprocess
 cat ../cache/fastq/pass/*fastq > ../cache/fastq/pass.fastq
 tombo preprocess annotate_raw_with_fastqs \
---fast5-basedir ../cache/ \
+--fast5-basedir ../cache/SINGLE_sample_data \
 --fastq-filenames ../cache/fastq/pass.fastq \
 --sequencing-summary-filenames ../cache/fastq/sequencing_summary.txt \
 --overwrite \
